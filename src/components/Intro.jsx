56d66c3a352d217fb45545af58e9927faad6e0a6
isahelheredia <isahelgersai@gmail.com>
2023-01-29 18:29:57 -0500

    Changes to text

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -7,7 +7,7 @@ const Intro = () => {
       <img className='rounded-full w-36 h-36' src={Avatar} alt='Extra large avatar'></img>
       <h1 className='text-4xl md:text-7xl mb-1 md:mb-3 font-bold'>Isahel Heredia</h1>
       <h2 className='text-3xl md:textxl mb-3 font-semibold'>Web Developer</h2>
-      <p className='text-xl md:textxl mb-3 font-medium'>React | CSS | JavaScript</p>
+      <p className='text-xl md:textxl mb-3 font-medium'>React | JavaScript | PHP</p>
       <p className='text-base max-w-xl mb-6 font-bold'>
         27 Years old software developer, with over 4+ years of work experience. Trying to keep in touch with current and
         emerging technologies and aiming to learn and improve my skills every day, building my version of the digital
