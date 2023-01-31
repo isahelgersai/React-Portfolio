ac10e7481b9b676114220a975c2ef2f52231751f
isahelheredia <isahelgersai@gmail.com>
2023-01-31 15:26:30 -0500

    select-none to title text

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 const Intro = () => {
   return (
     <div className='flex items-center justify-center flex-col text-center pt-20 pb-6 mb-56 mt-40'>
-      <h1 className='text-4xl lg:text-7xl md:text-7xl mb-1 md:mb-3 font-bold text-stone-50'>Isahel Heredia</h1>
-      <h2 className='text-2xl lg:text-5xl md:text-2xl mb-3 font-semibold text-teal-500'>Web Developer</h2>
-      <p className='text-xl md:text-1xl text mb-3 font-medium text-stone-50'>
-        React <span className='text-teal-500 text-2xl'>|</span> JavaScript{' '}
-        <span className='text-teal-500 text-2xl'>|</span> PHP
+      <h1 className='text-4xl lg:text-7xl md:text-7xl mb-1 md:mb-3 font-bold text-stone-50 select-none'>
+        Isahel Heredia
+      </h1>
+      <h2 className='text-2xl lg:text-5xl md:text-2xl mb-3 font-semibold text-teal-500 select-none'>Web Developer</h2>
+      <p className='text-xl md:text-1xl text mb-3 font-medium text-stone-50 select-none'>
+        React <span className='text-teal-500 text-2xl select-none'>|</span> JavaScript{' '}
+        <span className='text-teal-500 text-2xl select-none'>|</span> PHP
       </p>
     </div>
   );
