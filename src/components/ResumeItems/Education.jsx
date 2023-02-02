83c4f4f5353c8a3e8a02d090a1b0deec08b0ebc9
isahelheredia <isahelgersai@gmail.com>
2023-02-02 00:17:36 -0500

    Added icons to Resume component

--- src/components/ResumeItems/Education.jsx
+++ src/components/ResumeItems/Education.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { ReactComponent as IconWorld } from '../../assets/school.svg';
 const Education = () => {
   return (
     <div className='font-inter'>
-      <h5 className='text-xl font-semibold mt-10 text-stone-50 select-none'>Education</h5>
+      <div className='flex items-center mt-10 text-stone-50'>
+        <IconWorld className='w-8 h-8 fill-current' />
+        <h5 className='text-xl font-semibold ml-3 select-none'>Education</h5>
+      </div>
       <ol className='relative border-l border-teal-500 mt-10 mb-20 ml-10 '>
         <li className='mb-10 ml-4'>
           <div className='absolute w-3 h-4 bg-teal-500 rounded-full mt-1.5 -left-1.5 border-2 border-stone-50'></div>
--- src/components/ResumeItems/Work.jsx
+++ src/components/ResumeItems/Work.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import Title from '../Title';
+import { ReactComponent as IconBriefcase } from '../../assets/briefcase.svg';
 
 const Work = () => {
   return (
     <div className='font-inter'>
-      <h5 className='text-xl font-semibold mt-10 text-stone-50 select-none'>Work Experience</h5>
-
+      <div className='flex items-center mt-10 md:mr-8 md:mb-0 text-stone-50'>
+        <IconBriefcase className='w-8 h-8 fill-current' />
+        <h5 className='text-xl font-semibold ml-3 select-none'>Work Experience</h5>
+      </div>
       <ol className='relative border-l border-teal-500 mt-10 mb-20 ml-10 '>
         <li className='mb-10 ml-4'>
           <div className='absolute w-3 h-4 bg-teal-500 rounded-full mt-1.5 -left-1.5 border-2 border-stone-50'></div>
